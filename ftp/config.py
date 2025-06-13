fa4ec42aa8ef4e7277e012a569a2047380c64fc8
Benjamin <aodrenleberre@gmail.com>
2025-06-13 09:19:06 +0200

    Debut du projet

--- ftp/config.py
+++ ftp/config.py
@@ -1,7 +1,7 @@
 FTP_HOST = "127.0.0.1"
 FTP_PORT = 14148
 FTP_USER = "admin"
-FTP_PASS = "kasgruntESGI44!"
+FTP_PASS = "Respons11!"
 
 REGIONS = ["Paris", "Marseille", "Rennes", "Grenoble"]
-ROOT_DIR = "/chemin/vers/le/systeme/de/fichiers"
+ROOT_DIR = "C:/New_Tech"
